12fab8c1007e802740c6b3ec9046102e7d7d95ea
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 02:56:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(distance){
-let blocks = 0;
-return blocks = 42 - distance;
+function distanceFromHqInBlocks (blocks) {
+  if (blocks > 42) {
+    return blocks - 42;
+  } else {
+    return 42 - blocks;
+  }
+}
 
+function distanceFromHqInFeet (blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
 }
